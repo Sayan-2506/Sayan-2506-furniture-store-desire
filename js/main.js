957e661718c92802d4d55e12175bc250b3890b80
Sayan-2506 <61610731+Sayan-2506@users.noreply.github.com>
2022-02-22 11:02:22 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,98 @@
+$(function(){
+    $('.header__btn').on('click', function(){
+        $('.rightside-menu').removeClass('rightside-menu--close')
+    })
+    $('.rightside-menu__close').on('click', function(){
+        $('.rightside-menu').addClass('rightside-menu--close')
+    })
+
+    $('.top__slider').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+    })
+
+
+    $('.contact-slider').slick({
+        slidesToShow: 10,
+        slidesToScroll: 10,
+        dots: true,
+        arrows: false,
+        responsive: [
+            {
+              breakpoint: 1700,
+              settings: {
+                slidesToShow: 8,
+                slidesToScroll: 8,
+              }
+            },
+            {
+              breakpoint: 1611,
+              settings: {
+                slidesToShow: 6,
+                slidesToScroll: 6,
+              }
+            },
+            {
+              breakpoint: 922,
+              settings: {
+                slidesToShow: 5,
+                slidesToScroll: 5,
+              }
+            },
+            {
+              breakpoint: 770,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4,
+                dots: false,
+              }
+            },
+            {
+              breakpoint: 620,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                dots: false,
+              }
+            },
+            {
+              breakpoint: 470,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                dots: false,
+              }
+            },
+            {
+              breakpoint: 420,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false,
+              }
+            },
+        ]
+    })
+
+    $('.article-slider__box').slick({
+        prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/arrow-slide-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/arrow-slide-right.svg" alt=""></button>',
+    })
+
+    $('.header__btn-menu').on('click', function(){
+        $('.menu').toggleClass('menu--open')
+    })
+
+
+    if($(window).width() < 651){
+        $('.works-path__item--measuring').appendTo($('.works-path__items-box'))
+    }
+
+    var mixer = mixitup('.gallery__inner', {
+        load: {
+            filter: '.living'
+        }
+    });
+})
